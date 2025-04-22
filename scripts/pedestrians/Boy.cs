115c73f8f3f68275546eefb0a9a91bc59845222e
VanHansen <samu.kouhia@gmail.com>
2025-04-22 11:07:52 +0300

    Add Comments and regions to pedestrians

--- scripts/pedestrians/Boy.cs
+++ scripts/pedestrians/Boy.cs
@@ -3,8 +3,14 @@ using System;
 
 namespace Crosswalk
 {
+    /// <summary>
+    /// Boy is subclass of abstract class Pedestrian. It uses mostly base methods,
+    /// but it also randomly generates flight direction.
+    /// </summary>
     public partial class Boy : Pedestrian
     {
+        #region Public Properties
+
         [Export] public override float Speed { get; set; } = 40.0f;
         [Export] public override float RotationSpeed { get; set; } = 30000;
         [Export] public override float StopDuration { get; set; } = 3.0f;
@@ -12,17 +18,30 @@ namespace Crosswalk
         [Export] public override float SpeedTimer { get; set; } = 2.0f;
         [Export] public override float FlightDirection { get; set; }
 
+        #endregion
+
+        #region Private Properties
+
         private AnimatedSprite2D animatedSprite;
 
+        #endregion
+
+        #region Godot Built-In Methods
+
+        /// <summary>
+        /// Called when the node is added to the scene for the first time.
+        /// Initializes flight direction and sprite node.
+        /// </summary>
         public override void _Ready()
         {
             base._Ready();
             FlightDirection = GD.RandRange(-300, 300);
-
             animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
         }
 
-
+        /// <summary>
+        /// Called every frame. Manages flying behavior or default pedestrian behavior.
+        /// </summary>
         public override void _Process(double delta)
         {
             if (isFlying)
@@ -35,11 +54,22 @@ namespace Crosswalk
             }
         }
 
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Handles logic when the pedestrian is hit by a car.
+        /// Starts flying animation and resets fly timer.
+        /// </summary>
+        /// <param name="car">The car that collided with the pedestrian.</param>
         protected override void HandleCarCollision(Car car)
         {
             isFlying = true;
             GD.Print("Boy was hit by a car :[");
             FlyTime = 0;
         }
+
+        #endregion
     }
 }
--- scripts/pedestrians/Woman.cs
+++ scripts/pedestrians/Woman.cs
@@ -3,8 +3,14 @@ using System;
 
 namespace Crosswalk
 {
+    /// <summary>
+    /// Woman is a subclass of abstract class Pedestrian. It uses mostly base methods,
+    /// but it also randomly generates flight direction.
+    /// </summary>
     public partial class Woman : Pedestrian
     {
+        #region Public Properties
+
         [Export] public override float Speed { get; set; } = 40.0f;
         [Export] public override float RotationSpeed { get; set; } = 30000;
         [Export] public override float StopDuration { get; set; } = 3.0f;
@@ -12,17 +18,31 @@ namespace Crosswalk
         [Export] public override float SpeedTimer { get; set; } = 2.0f;
         [Export] public override float FlightDirection { get; set; }
 
+        #endregion
+
+        #region Private Properties
+
         private AnimatedSprite2D animatedSprite;
 
+        #endregion
+
+        #region Godot Built-In Methods
+
+        /// <summary>
+        /// Called when the node enters the scene tree for the first time.
+        /// Initializes flight direction and retrieves the animated sprite node.
+        /// </summary>
         public override void _Ready()
         {
             base._Ready();
             FlightDirection = GD.RandRange(-300, 300);
-
             animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
         }
 
-
+        /// <summary>
+        /// Called every frame. Handles flying or walking logic based on character state.
+        /// </summary>
+        /// <param name="delta">Time elapsed since the last frame.</param>
         public override void _Process(double delta)
         {
             if (isFlying)
@@ -35,11 +55,22 @@ namespace Crosswalk
             }
         }
 
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Handles logic when the woman is hit by a car.
+        /// Enables flying behavior and resets flying timer.
+        /// </summary>
+        /// <param name="car">The car that collided with the pedestrian.</param>
         protected override void HandleCarCollision(Car car)
         {
             isFlying = true;
-            GD.Print("Man was hit by a car :]");
+            GD.Print("Woman was hit by a car :[]");
             FlyTime = 0;
         }
+
+        #endregion
     }
 }
